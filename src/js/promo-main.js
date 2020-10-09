7cc15ac7f5b45ac866d4132c7911be4cfafc51be
IsleBeeGun <IsleBeeGun@gmail.com>
2020-10-09 15:31:55 +0300

    fixed faq section

--- src/js/promo-main.js
+++ src/js/promo-main.js
@@ -1,8 +1,11 @@
 import '../styles/promo-main.scss';
 import './burger-menu';
 import { handleFormByID } from './forms-logic';
-import 'zenscroll';
+import zenScroll from 'zenscroll';
 
 // Setup forms
 const formDesktop = handleFormByID('form-desktop');
 const formMobile = handleFormByID('form-mobile');
+
+// Setup scroll
+zenScroll.setup(null, 0); // setting top offset to zero
